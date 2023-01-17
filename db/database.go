eb9154f4bb917060ef8fd0bc1e7064dbd2023c86
MrApichat <121045183+MrApichat@users.noreply.github.com>
2023-01-18 03:53:28 +0700

    delete unused

--- db/database.go
+++ db/database.go
@@ -15,13 +15,11 @@ func Con() {
 	// connection := "postgres://postgres:12345678@database-1.ckhl0w0gyple.ap-northeast-1.rds.amazonaws.com/p2p_go"
 	Db, err =  sql.Open("postgres", "postgres://postgres:123456@localhost/p2p-go?sslmode=disable")
 	if err != nil {
-		fmt.Println("here")
 		log.Fatal(err)
 	}
 	err = Db.Ping()
 
 	if err != nil {
-		fmt.Println("here2")
 		log.Fatal(err)
 	}
 }
