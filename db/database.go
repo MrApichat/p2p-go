8fd7290738b46a2cb4f0ff313880b1a6f31db58f
MrApichat <121045183+MrApichat@users.noreply.github.com>
2023-01-18 03:53:51 +0700

    delete unused

--- db/database.go
+++ db/database.go
@@ -2,10 +2,9 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 
-	"log"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 var Db *sql.DB
@@ -13,7 +12,7 @@ var Db *sql.DB
 func Con() {
 	var err error
 	// connection := "postgres://postgres:12345678@database-1.ckhl0w0gyple.ap-northeast-1.rds.amazonaws.com/p2p_go"
-	Db, err =  sql.Open("postgres", "postgres://postgres:123456@localhost/p2p-go?sslmode=disable")
+	Db, err = sql.Open("postgres", "postgres://postgres:123456@localhost/p2p-go?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
