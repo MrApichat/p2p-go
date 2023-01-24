d28740100f6359cc4e7c885cdd9187ce2d51578d
MrApichat <121045183+MrApichat@users.noreply.github.com>
2023-01-25 03:13:07 +0700

    Update TradeOrderModel.go

--- models/TradeOrderModel.go
+++ models/TradeOrderModel.go
@@ -1,20 +1,20 @@
 package models
 
 type TradeOrderModel struct {
-	Id int64
-	User DisplayUserModel
-	MerchantOrder MerchantOrderModel
-	Amount float64
-	PaymentMethod PaymentMethodModel
-	TotalPrice float64
-	Status TradeStatus
+	Id            int64              `json:"id"`
+	User          DisplayUserModel   `json:"user"`
+	MerchantOrder MerchantOrderModel `json:"merchant_order"`
+	Amount        float64            `json:"amount"`
+	PaymentMethod PaymentMethodModel `json:"payment_method"`
+	TotalPrice    float64            `json:"total_price"`
+	Status        TradeStatus        `json:"status"`
 }
 
 type TradeStatus string
 
 const (
-	Open TradeStatus = "open" //open order waiting to trade fiat
-	Waiting TradeStatus = "waiting" //already sent fiat waiting coin owner confirm
+	Open     TradeStatus = "open"     //open order waiting to trade fiat
+	Waiting  TradeStatus = "waiting"  //already sent fiat waiting coin owner confirm
 	Complete TradeStatus = "complete" //trader and merchant recieve what they want
-	Cancel TradeStatus = "cancel" //you can cancel order when order is create only
+	Cancel   TradeStatus = "cancel"   //you can cancel order when order is create only
 )
